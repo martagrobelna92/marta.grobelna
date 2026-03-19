b51c12542156665239a93416c04cd0d2fade363c
martagrobelna92 <marta.grobelna@hotmail.de>
2026-03-19 12:18:30 +0000

    Deploying to gh-pages from @ martagrobelna92/marta.grobelna@b09892f4410cf2f161b80f2506bd131c4197c45d 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -63,46 +63,11 @@ ninja.data = [{
           section: "Teachings",handler: () => {
               window.location.href = "/marta.grobelna/teachings/introduction-to-machine-learning/";
             },},{
-        id: 'social-cv',
-        title: 'CV',
-        section: 'Socials',
-        handler: () => {
-          window.open("/marta.grobelna/assets/pdf/example_pdf.pdf", "_blank");
-        },
-      },{
         id: 'social-email',
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
-        },
-      },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
-        },
-      },{
-        id: 'social-rss',
-        title: 'RSS Feed',
-        section: 'Socials',
-        handler: () => {
-          window.open("/marta.grobelna/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("mailto:%6D%61%72%74%61.%67%72%6F%62%65%6C%6E%61@%74%75%6D.%64%65", "_blank");
         },
       },{
       id: 'light-theme',
